04948909b32f9ec2ae4abc34db0cd3e6708a6ed8
Maksym Kryvonos <kryma937@student.otago.university.ac.nz>
2023-03-17 18:36:26 +1300

    Made the window not resizable and centered widgets

--- gui.py
+++ gui.py
@@ -9,20 +9,25 @@ def browse_file():
 # Create the main window
 root = tk.Tk()
 root.title("Note Taker")
+root.resizable(False, False)
+
+# Create a frame to hold the widgets
+frame = tk.Frame(root)
+frame.pack(expand=True)
 
 # Create and place the labels
-tk.Label(root, text="From page:").grid(row=0, column=0, sticky=tk.W)
-tk.Label(root, text="To page:").grid(row=1, column=0, sticky=tk.W)
+tk.Label(frame, text="From page:").grid(row=0, column=0, sticky=tk.W)
+tk.Label(frame, text="To page:").grid(row=1, column=0, sticky=tk.W)
 
 # Create and place the entry fields
-entry1 = tk.Entry(root)
+entry1 = tk.Entry(frame)
 entry1.grid(row=0, column=1)
-entry2 = tk.Entry(root)
+entry2 = tk.Entry(frame)
 entry2.grid(row=1, column=1)
 
 # Create and place the drop-down selector (combobox)
 options = ["Automatic title", "Write your own title"]
-combobox = ttk.Combobox(root, values=options)
+combobox = ttk.Combobox(frame, values=options)
 combobox.set("Select an option")
 combobox.grid(row=2, column=0, columnspan=2)
 
@@ -31,7 +36,7 @@ default_option = "Automatic title"
 combobox.set(default_option)
 
 # Create and place the browse file button
-browse_button = tk.Button(root, text="Browse", command=browse_file)
+browse_button = tk.Button(frame, text="Browse", command=browse_file)
 browse_button.grid(row=3, column=0, columnspan=2)
 
 # Start the main event loop
